a137e40916b469d348de9e9ee6dda99d6ef4ad52
Shane Gallatin <shane.gallatin@verizon.net>
2019-10-28 16:53:18 -0500

    first page and drop-down

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      menuStatus: "menu-close"
+    }
+  }
+  handleClick = e => {
+    if (this.state.menuStatus === "menu-close"){
+      this.setState({menuStatus: "menu-open"})
+    } else {
+      this.setState({menuStatus: "menu-close"});
+    }
+  };
+  render(){
+    return (
+      <div className="back-image">
+        <nav>
+          <h1>Start Bootstrap</h1>
+          <img 
+            src="https://icon-library.net/images/menu-icon-white-png/menu-icon-white-png-27.jpg"
+            alt="ham-btn"
+            onClick={this.handleClick}/>
+          <ul>
+            <li>SERVICES</li>
+            <li>PORTFOLIO</li>
+            <li>ABOUT</li>
+            <li>TEAM</li>
+            <li>CONTACT</li>
+          </ul>
+        </nav>
+        <menu id="menu" className={this.state.menuStatus}>
+          <ul className="menu-bar">
+           
+          </ul>
+        </menu>
+        <div className="welcome">
+        <h2>Welcome To Our Studio!</h2>
+        <h3>IT'S NICE TO MEET YOU</h3>
+        <button>TELL ME MORE</button>
+        </div>
+      </div>
+      
+    );
+
+  }
 }
 
 export default App;
